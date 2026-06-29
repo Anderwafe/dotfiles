7396cc95be38a59f86baf75ccce4cbdb76cc80fa
Anderwafe <Anderwafe@yandex.by>
2026-06-29 22:55:40 +0300

    alt+shift keymap toggle

--- someDefaults.sh
+++ someDefaults.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-sudo localectl set-x11-keymap us,ru pc105 altgr-intl,winkeys grp:lctrl_lshift_toggle,compose:ralt,lv3:switch
+sudo localectl set-x11-keymap us,ru pc105 altgr-intl,winkeys grp:lalt_lshift_toggle,compose:ralt,lv3:switch
